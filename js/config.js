a86ec77f672528ad90250318a627267f03d4f47d
gestaodecustohrdjsn <gestaodecustohrdjsn@gmail.com>
2026-09-16 10:40:30 -0400

    Update APP_VERSION to 1.1.1

--- js/config.js
+++ js/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  APP_VERSION: "1.1.0",
+  APP_VERSION: "1.1.1",
   APP_NAME: "Pesquisa de Satisfação - HRPP - Altas",
 
   // Cole aqui a URL /exec do Web App do Google Apps Script.
